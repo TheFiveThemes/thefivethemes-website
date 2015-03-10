09fb7a346d975be6fd1b0cc87ea6f698acca71dc
Hugo Baeta <hugo@baeta.me>
2015-03-09 19:31:04 -0700

    Styled Comments & Form

--- content-page.php
+++ content-page.php
@@ -21,7 +21,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'thefivethemes' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -24,11 +24,13 @@ function the_posts_navigation() {
 		<div class="nav-links">
 
 			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'thefivethemes' ) ); ?></div>
+			<div class="nav-previous">
+				<?php next_posts_link( __( 'Older posts', 'thefivethemes' ) ); ?></div>
 			<?php endif; ?>
 
 			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'thefivethemes' ) ); ?></div>
+			<div class="nav-next">
+				<?php previous_posts_link( __( 'Newer posts', 'thefivethemes' ) ); ?></div>
 			<?php endif; ?>
 
 		</div><!-- .nav-links -->
